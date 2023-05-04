28e6a221fbcdc32a96bb7bf91674d83100c387d7
Erick <ericksettidossantos@hotmail.com>
2023-05-04 00:42:02 -0300

    Estudos sobre JS consumindo e tratando dados de uma API

--- sites/JS consumindo e tratando dados API/script.js
+++ sites/JS consumindo e tratando dados API/script.js
@@ -1,3 +1,29 @@
-var consultaCEP = fetch('https://viacep.com.br/ws/01001000/json/')
+async function buscaEndereco(cep) {
+    var mensagemErro = document.getElementById('erro')
+    mensagemErro.innerHTML = ""
+    try {
+        var consultaCEP = await fetch(`https://viacep.com.br/ws/${cep}/json/`)
+        var consultaCEPConvertida = await consultaCEP.json()
+        if (consultaCEPConvertida.erro) {
+            throw Error("CEP nao existente!")
+        }
+        var cidade = document.getElementById('cidade')
+        var logradouro = document.getElementById('endereco')
+        var estado = document.getElementById('estado')
 
-console.log (consultaCEP)
+        cidade.value = consultaCEPConvertida.localidade
+        logradouro.value = consultaCEPConvertida.logradouro
+        estado.value = consultaCEPConvertida.uf
+        bairro.value = consultaCEPConvertida.bairro
+
+
+        console.log(consultaCEPConvertida)
+        return consultaCEPConvertida
+    } catch (erro) {
+        mensagemErro.innerHTML = `<p>CEP Invalido. Tente novamente</p>`
+        console.log(erro)
+    }
+}
+
+var cep = document.getElementById('cep')
+cep.addEventListener("focusout", () => buscaEndereco(cep.value))
